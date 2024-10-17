366c6e17466de5c1b495f3288f7151d8bacaaee9
Rui Alberto Reinehr Filho <rui.reinehr@gmail.com>
2024-10-17 09:09:25 -0300

    res

---
+++ exercícios/es.js
@@ -0,0 +1,10 @@
+malo = [0,4,7,9,6]
+malo.push(1)
+malo.sort()
+malo.indexOf(4)
+for(os in malo){
+    console.log(`No indice ${os} temos o valor ${malo[os]}`
+
+    )
+    console.log(malo.indexOf(1))
+}
